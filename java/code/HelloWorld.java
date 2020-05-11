06f9dab384fafe0e23ebebe6b201efc0bc3bb191
ronghon <ronghon1969@gmail.com>
2020-05-11 22:53:35 +0800

    新增java及復習jquery  domapi

---
+++ java/code/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {  //HelloWorld必須需檔案名稱一模一樣包含大小寫
+// 定義一個類的名稱,類是Java中所有源代碼的基本組織單位
+  public static void main(String[] args) {
+    // 程序執行的起點(固定不變的寫法),main方法
+    System. out.println("Hello, World!!!");
+    //輸出語句(螢幕顯示)
+  }
+}
