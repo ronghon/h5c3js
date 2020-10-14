d423c61df73a2b1d25c7892591898a420e1303d5
ronghon <ronghon1969@gmail.com>
2020-10-14 22:18:06 +0800

    java基礎in home

--- java/code/HelloWorld.java
+++ java/code/HelloWorld.java
@@ -1,8 +1,6 @@
-public class HelloWorld {  //HelloWorld必須需檔案名稱一模一樣包含大小寫
-// 定義一個類的名稱,類是Java中所有源代碼的基本組織單位
-  public static void main(String[] args) {
-    // 程序執行的起點(固定不變的寫法),main方法
-    System. out.println("Hello, World!!!");
-    //輸出語句(螢幕顯示)
-  }
+public class HelloWorld {
+    // test this is a JAVA
+    public static void main(String[] args){
+        System.out.println("Hello JAVA-1!!");
+    }
 }
